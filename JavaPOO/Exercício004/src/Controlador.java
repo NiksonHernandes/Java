fbaa9e3a37282801d908361be7f312526171ac98
Nikson Hernandes <79341026+NiksonHernandes@users.noreply.github.com>
2022-08-27 19:04:50 -0300

    Exercício 004 - Encapsulamento

---
+++ JavaPOO/Exercício004/src/Controlador.java
@@ -0,0 +1,14 @@
+public interface Controlador {
+	//metodos abstratos, ou seja, não serão desenvolvidos aqui 
+	public abstract void ligar();
+	public abstract void desligar();
+	public abstract void abrirMenu();
+	public abstract void fecharMenu();
+	public abstract void maisVolume();
+	public abstract void menosVolume();
+	public abstract void ligarMudo();
+	public abstract void desligarMudo();
+	public abstract void play();
+	public abstract void pause();
+	
+}
